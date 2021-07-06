6739ccfc64bf0a9a7001a057d73cb019cd222dd2
morita <moritasoushi@gmail.com>
2021-07-07 08:43:16 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-# change shell
-/usr/bin/grep /usr/local/bin/zsh /etc/shells && :
-if [ "$?" -ne 0 ]; then
-    echo "homebrewのzshに変更します"
-    sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
-    /usr/bin/chsh -s "$(brew --prefix)/bin/zsh"
-fi
-
 # install brew
-
 # brewのインストール
 if [ ! -e /usr/local/bin/brew ]; then
   echo "homebrewをインストールします"
@@ -19,9 +10,21 @@ fi
 # brew bundle 叩く
 echo "Brewfileにリストされているアプリケーションをインストールします"
 brew bundle --file './Brewfile'
+
+echo "BrewfileにリストされているアプリケーションをClean Upします"
 brew bundle cleanup --file './Brewfile'
 
 # brew doctorで状態確認
 echo
 echo 'homebrewの状態を確認します'
 brew doctor
+
+# change shell
+/usr/bin/grep /usr/local/bin/zsh /etc/shells && :
+if [ "$?" -ne 0 ]; then
+  echo "homebrewのzshに変更します"
+  sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
+  /usr/bin/chsh -s "$(brew --prefix)/bin/zsh"
+fi
+
+source ./deploy.sh
