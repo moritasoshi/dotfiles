a3ae8d1592a0f8552fe113edeb2b303489eb90af
soshi morita <moritasoushi@gmail.com>
2021-12-15 21:49:24 +0900

    add some aliases

--- home/zsh/alias.zsh
+++ home/zsh/alias.zsh
@@ -1,8 +1,18 @@
-# エイリアス
 alias l='ls -F'
 alias la='ls -a'
 alias ll='ls -l'
 
+alias f='find . -type f'
+alias grep='grep --color=auto'
+alias g='git'
+
+alias sz='source ~/.zshrc'
+alias path='echo $PATH'
+
 alias idea='open -na "IntelliJ IDEA.app" --args "$@"'
 alias code='open -na "Visual Studio Code.app" --args "$@"'
 alias subl='(){/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl $1}'
+
+# Global aliases
+alias -g G='| grep'
+alias -g X='| xargs'
