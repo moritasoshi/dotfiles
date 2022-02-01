7c300d9d995caaf19e8376597c20885531593c68
morita <moritasoushi@gmail.com>
2022-02-01 17:05:19 +0900

    shellcheck導入してみた

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 WORK_DIR=$(cd $(dirname $0); pwd)
 
-function install_brew() {
+install_brew() {
   if [ ! -e /usr/local/bin/brew ]; then
     echo "+++++Install Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 }
 
-function install_ruby() {
-  if [ ! -e $HOME/.rvm/bin]; then
+install_ruby() {
+  if [ ! -e $HOME/.rvm/bin ]; then
     echo "+++++Install RVM..."
     curl -sSL https://get.rvm.io | bash -s stable
-    source $HOME/.rvm/scripts/rvm
+    . "$HOME"/.rvm/scripts/rvm
   fi
 }
 
-function setup_brew() {
+setup_brew() {
   echo "+++++Install Homebrew packages..."
   brew bundle --file "$WORK_DIR"/etc/homebrew/Brewfile
   echo "+++++Clean up Homebrew packages..."
@@ -25,7 +25,7 @@ function setup_brew() {
   brew doctor
 }
 
-function shell_check() {
+shell_check() {
   /usr/bin/grep /usr/local/bin/zsh /etc/shells && :
   if [ "$?" -ne 0 ]; then
     echo "+++++homebrewのzshに変更します"
