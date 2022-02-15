b713a1c0c6171c5678e30dbeaac06553bf2a954f
morita <moritasoushi@gmail.com>
2022-02-15 17:48:15 +0900

    alpha

---
+++ nvim/.config/nvim/lua/moritasoshi/lsp/settings/sumneko_lua.lua
@@ -0,0 +1,31 @@
+local M = {}
+M.setup = function(opts) -- opts = {on_attach: on_attach, capabilities: capabilities}
+  local on_attach = opts.on_attach
+  local capabilities = opts.capabilities
+
+  return require("lua-dev").setup({
+    library = {
+      plugins = true
+    },
+    lspconfig = {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {'vim'},
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
+    }
+  })
+end
+
+return M
