413633e08868d6438c3c5f3bfe0913b7a4e2b725
morita <moritasoushi@gmail.com>
2022-02-22 17:09:24 +0900

    update

--- nvim/.config/nvim/lua/moritasoshi/options.lua
+++ nvim/.config/nvim/lua/moritasoshi/options.lua
@@ -2,11 +2,13 @@
 local command = vim.cmd
 local opt = vim.opt
 
--- vim.opt.termguicolors = true
--- vim.g.gruvbox_material_palette = 'mix'
--- vim.g.gruvbox_material_background = 'hard'
--- command [[colorscheme gruvbox-material]]
-command [[colorscheme darcula]]
+vim.opt.termguicolors = true
+vim.g.gruvbox_material_palette = 'mix'
+vim.g.gruvbox_material_background = 'hard'
+vim.g.gruvbox_material_enable_italic = 1
+command [[colorscheme gruvbox-material]]
+-- command [[colorscheme darcula]]
+-- require('nightfox').load("nordfox")
 command [[syntax on]]
 
 -- autocmd ColorScheme * highlight Normal ctermbg = black
