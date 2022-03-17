1150592060bfa98b9b305fb6c7074f5527022bba
morita <moritasoushi@gmail.com>
2022-03-17 10:08:58 +0900

    自動でIME切り替え

--- nvim/.config/nvim/after/plugin/im_select.lua
+++ nvim/.config/nvim/after/plugin/im_select.lua
@@ -1,47 +1,22 @@
---[=====[
-## 準備
-- im-select をinstall
-brew tap daipeihust/tap && brew install im-select
+--[[--
+  Summary. Vimへのフォーカス取得と挿入モード離脱時にひらがな から 英字 にIME変換する
+
+  Requirements. im-select must be installed.
+
+  Reference. https://github.com/brglng/vim-im-select
 
 
-## やりたいこと
-- normal mode で必ず Roman
-- insert mode で以前の 言語
-- insert mode 中のIMEを保存する
+  @todo
+  InsertEnter, FocusLost時に元のIMEにswitch-backする?
+]]
 
---]=====]
+local ja = "com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese"
+local en = "com.apple.inputmethod.Kotoeri.RomajiTyping.Roman"
 
--- local to_ja = string.format(":call system('%s')", "com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese")
--- local to_us = string.format(":call system('%s')", "com.apple.inputmethod.Kotoeri.RomajiTyping.Roman")
---
--- vim.api.nvim_create_augroup("MacImSelect", { clear = true })
--- vim.api.nvim_create_autocmd("TextYankPost", {
---   cmd = vim.cmd([[vim]]),
---   callback = function()
---     vim.highlight.on_yank { higroup = "Substitute", timeout = 150, on_macro = true }
---   end,
---   group = "MacImSelect",
--- })
-
-vim.cmd([[
-    augroup win_ime_con
-        autocmd!
-        autocmd BufEnter * let g:mac_ime_con_is_insert = 0
-        autocmd InsertEnter * call system('im-select com.apple.inputmethod.Kotoeri.RomajiTyping.Roman')
-        autocmd InsertLeave * call system('im-select com.apple.inputmethod.Kotoeri.RomajiTyping.Roman')
-    augroup END
-]])
-
-local saveMode = function(mode)
-  vim.g.ime_mode = mode
-end
-
--- modeからim codeに変換する
-local convToCode = function()
-  local mode = vim.g.ime_mode
-  local prefix = "com.apple.inputmethod.Kotoeri.RomajiTyping."
-  if mode == 1 then
-    return prefix .. "Japanese"
-  end
-  return prefix .. "Roman"
-end
+vim.api.nvim_create_augroup("_MSImSelect", { clear = true })
+vim.api.nvim_create_autocmd({ "FocusGained", "InsertLeave" }, {
+  callback = function()
+    vim.cmd(string.format("call system('im-select %s')", en))
+  end,
+  group = "_MSImSelect",
+})
