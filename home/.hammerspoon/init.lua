b15bb8b22b1ba0e96533b3af98903c3183daa928
morita <moritasoushi@gmail.com>
2022-03-20 18:24:18 +0900

    alacrittyにHotkeyを設定

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,171 +1,31 @@
--- tutolial
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "w", function()
-  hs.alert.show("Hello World!")
-end)
+--- open Alacritty
+--[[
+hs.window < hs.space < hs.screen
+window: アプリケーション毎のウィンドウ
+space : ディスプレイ毎にあるワークスペース
+screen: ディスプレイ(macbook本体, Dellのモニターなど)
+--]]
 
---
--- -- https://github.com/ipatch/dotfiles/blob/master/jobs/macos/Users/mr-fancy/hammerspoon/ctrlDoublePress.lua
--- local timer = require("hs.timer")
--- local eventtap = require("hs.eventtap")
---
--- local events = eventtap.event.types
--- local module = {}
--- local spaces = require("hs._asm.undocumented.spaces")
---
--- -- double tap の間隔[s]
--- module.timeFrame = 1
---
---
--- function MoveFullScreenWindow(app)
---     local activeSpace = spaces.activeSpace()
---     local win = app:focusedWindow()
---     win = win:toggleFullScreen()
---     win = win:toggleFullScreen()
---     app:hide()
---     win:spacesMoveTo(activeSpace)
---     win:focus()
--- end
---
--- -- double tap で toggle で kitty を表示/非表示する
--- module.action = function()
---     local appName = "alacritty"
---     local app = hs.application.get(appName)
---
---     if app == nil then
---         hs.application.launchOrFocus(appName)
---     elseif app:isFrontmost() then
---         app:hide()
---     else -- すでに存在する場合、window を activeSpace に移動させて focus する
---         local activeSpace = spaces.activeSpace()
---         local win = app:focusedWindow()
---         win:spacesMoveTo(activeSpace)
---         hs.application.launchOrFocus(appName)
---     end
--- end
---
---
--- hs.hotkey.bind({"ctrl"}, "m", function()
---     local appName = "Code"
---     local activeSpace = spaces.activeSpace()
---     local app = hs.application.find(appName)
---     if app == nil then
---         hs.application.launchOrFocus("Visual Studio Code")
---     elseif app:isFrontmost() then
---         app:hide()
---     else
---         local win = app:focusedWindow()
---         win:spacesMoveTo(activeSpace)
---         hs.application.launchOrFocus("Visual Studio Code")
---     end
--- end)
---
---
--- -- module.action = function()
--- --     local appName = "Alacritty"
--- --     local activeSpace = spaces.activeSpace()
--- --     local app = hs.application.find(appName)
--- --     if app == nil then
--- --         hs.application.launchOrFocus(appName)
--- --     elseif app:isFrontmost() then
--- --         app:hide()
--- --     else
--- --         local win = app:focusedWindow()
--- --         win:spacesMoveTo(activeSpace)
--- --         hs.application.launchOrFocus(appName)
--- --     end
--- -- end
---
--- -- hs.hotkey.bind({"ctrl"}, "m", function()
--- --     local appName = "Spotify"
--- --     local activeSpace = spaces.activeSpace()
--- --     local app = hs.application.find(appName)
--- --     if app == nil then
--- --         hs.application.launchOrFocus(appName)
--- --     elseif app:isFrontmost() then
--- --         app:hide()
--- --     else
--- --         local win = app:focusedWindow()
--- --         win:spacesMoveTo(activeSpace)
--- --         hs.application.launchOrFocus(appName)
--- --     end
--- -- end)
---
--- hs.hotkey.bind({"ctrl"}, ",", function()
---     local appName = "Slack"
---     local activeSpace = spaces.activeSpace()
---     local app = hs.application.find(appName)
---     if app == nil then
---         hs.application.launchOrFocus(appName)
---     elseif app:isFrontmost() then
---         app:hide()
---     else
---         local win = app:focusedWindow()
---         win:spacesMoveTo(activeSpace)
---         hs.application.launchOrFocus(appName)
---     end
--- end)
---
---
--- hs.hotkey.bind({"ctrl"}, ".", function()
---     local appName = "Google Chrome"
---     local activeSpace = spaces.activeSpace()
---     local app = hs.application.find(appName)
---     if app == nil then
---         hs.application.launchOrFocus(appName)
---     elseif app:isFrontmost() then
---         app:hide()
---     else
---         local win = app:focusedWindow()
---         win:spacesMoveTo(activeSpace)
---         hs.application.launchOrFocus(appName)
---     end
--- end)
---
---
--- local timeFirstControl, firstDown, secondDown = 0, false, false
--- local noFlags = function(ev)
---     local result = true
---     for _, v in pairs(ev:getFlags()) do
---         if v then
---             result = false
---             break
---         end
---     end
---     return result
--- end
---
--- -- control だけ押されているか確認. 例えば shift+control 等は無視するようにする
--- local onlyCtrl = function(ev)
---     local result = ev:getFlags().ctrl
---     for k, v in pairs(ev:getFlags()) do
---         if k ~= "ctrl" and v then
---             result = false
---             break
---         end
---     end
---     return result
--- end
---
--- -- module.timeFrame 秒以内に2回 ctrl を押したらダブルタップとみなす
--- module.eventWatcher = eventtap.new({events.flagsChanged, events.keyDown}, function(ev)
---     if (timer.secondsSinceEpoch() - timeFirstControl) > module.timeFrame then
---         timeFirstControl, firstDown, secondDown = 0, false, false
---     end
---
---     if ev:getType() == events.flagsChanged then
---         if noFlags(ev) and firstDown and secondDown then
---             if module.action then module.action() end
---             timeFirstControl, firstDown, secondDown = 0, false, false
---         elseif onlyCtrl(ev) and not firstDown then
---             firstDown = true
---             timeFirstControl = timer.secondsSinceEpoch()
---         elseif onlyCtrl(ev) and firstDown then
---             secondDown = true
---         elseif not noFlags(ev) then
---             timeFirstControl, firstDown, secondDown = 0, false, false
---         end
---     else
---         timeFirstControl, firstDown, secondDown = 0, false, false
---     end
---     return false
--- end):start()
+local application = require("hs.application")
+local spaces = require("hs.spaces")
+
+local double_press = require("ctrlDoublePress")
+
+local open_alacritty = function()
+  local appName = "Alacritty"
+  local app = application.get(appName)
+
+  if app == nil then
+    application.launchOrFocus(appName)
+  elseif app:isFrontmost() then
+    app:hide()
+  else
+    local app_window = app:focusedWindow()
+    local focused_space = spaces.focusedSpace()
+    spaces.moveWindowToSpace(app_window, focused_space)
+    app:setFrontmost()
+  end
+end
+
+double_press.timeFrame = 0.5
+double_press.action = open_alacritty
