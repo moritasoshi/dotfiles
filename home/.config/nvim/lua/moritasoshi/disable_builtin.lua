55e1e9e0339af657f9b0d46cfe5887e720795606
morita <moritasoushi@gmail.com>
2022-03-28 17:02:24 +0900

    Disable nvim builtin plugins

--- home/.config/nvim/lua/moritasoshi/disable_builtin.lua
+++ home/.config/nvim/lua/moritasoshi/disable_builtin.lua
@@ -18,3 +18,16 @@ vim.g.loaded_rrhelper = 1
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
+
+vim.g.loaded_man = 1
+vim.g.loaded_remote_plugins = 1
+vim.g.loaded_shada_plugin = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.skip_loading_mswin = 1
+
+vim.g.did_install_default_menus = 1
+vim.g.did_install_syntax_menu = 1
+vim.g.did_indent_on = 1
+vim.g.did_load_filetypes = 1
+vim.g.did_load_ftplugin = 1
