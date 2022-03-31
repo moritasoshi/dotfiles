c2a4ed71c57f3be23ee8f74b1e3d4aeb9097e7e5
morita <moritasoushi@gmail.com>
2022-03-31 23:10:02 +0900

    Blinking Cursor

--- home/.config/nvim/lua/moritasoshi/options.lua
+++ home/.config/nvim/lua/moritasoshi/options.lua
@@ -19,6 +19,11 @@ augroup END
 ]]
 
 opt.syntax = "on"
+command([[
+set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
+\,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor
+\,sm:block-blinkwait175-blinkoff150-blinkon175
+]]) -- Blinking cursor
 
 opt.encoding = "utf-8"
 opt.fileencodings = "utf-8,sjis,euc-jp,latin"
