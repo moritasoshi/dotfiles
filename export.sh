de7cb16ec5f4bd4f9d6b659f8ab566974693bd81
morita <moritasoushi@gmail.com>
2022-04-14 14:36:50 +0900

    refactor(spectacle): No longer use Spectacle

--- export.sh
+++ export.sh
@@ -3,15 +3,8 @@
 set -e
 
 BREW_DIR=~/src/dotfiles/etc/homebrew/
-SPECTACLE_DIR=~/src/dotfiles/etc/spectacle/
-SPECTACLE_HOME=~/Library/Application\ Support/Spectacle/
 
 # export Brewfile
 pushd "$BREW_DIR"
 brew bundle dump --force
-popd
-
-# export Spectacle Sortcuts.json
-pushd "$SPECTACLE_HOME"
-cp Shortcuts.json "$SPECTACLE_DIR"
 popd
