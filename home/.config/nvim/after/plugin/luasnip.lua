984661217807508f8226bfc3b39c99a02d97d444
morita <moritasoushi@gmail.com>
2022-04-28 17:06:19 +0900

    chore(nvim): Add some snippets

--- home/.config/nvim/after/plugin/luasnip.lua
+++ home/.config/nvim/after/plugin/luasnip.lua
@@ -4,12 +4,16 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
-ls.add_snippets("typescriptreact", {
+ls.add_snippets("javascript", {
   s("cl", {
     t("console.log("),
     i(1, "msg"),
     t(")"),
   }),
 })
+
+ls.filetype_extend("typescript", { "javascript" })
+ls.filetype_extend("typescriptreact", { "javascript" })
+
 
 require("luasnip.loaders.from_vscode").lazy_load() -- Activate friendly snippets
