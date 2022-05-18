00508c1517d2c1a89b6d63e7fdc0dac0b1b47d5d
morita <moritasoushi@gmail.com>
2022-05-18 17:02:17 +0900

    feat(hlslens): update

--- home/.config/nvim/lua/moritasoshi/config/hlslens.lua
+++ home/.config/nvim/lua/moritasoshi/config/hlslens.lua
@@ -1,5 +1,33 @@
 require("hlslens").setup {
-  calm_down = true,
-  nearest_only = true,
-  nearest_float_when = "always",
+  override_lens = function(render, posList, nearest, idx, relIdx)
+    local sfw = vim.v.searchforward == 1
+    local indicator, text, chunks
+    local absRelIdx = math.abs(relIdx)
+    local prev, next = "N", "n"
+    -- local prev, next = "▲", "▼"
+
+    if absRelIdx > 1 then
+      indicator = ("%d%s"):format(absRelIdx, sfw ~= (relIdx > 1) and prev or next)
+    elseif absRelIdx == 1 then
+      indicator = sfw ~= (relIdx == 1) and prev or next
+    else
+      indicator = ""
+    end
+
+    local lnum, col = unpack(posList[idx])
+    if nearest then
+      local cnt = #posList
+      if indicator ~= "" then
+        text = ("[%s %d/%d]"):format(indicator, idx, cnt)
+      else
+        text = ("[%d/%d]"):format(idx, cnt)
+      end
+      chunks = { { " ", "Ignore" }, { text, "HlSearchLensNear" } }
+    else
+      -- text = ('[%s %d]'):format(indicator, idx)
+      -- chunks = {{' ', 'Ignore'}, {text, 'HlSearchLens'}}
+      chunks = {}
+    end
+    render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+  end,
 }
