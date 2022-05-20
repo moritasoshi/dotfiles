a46d508fd07ccfc4d0396de4737f1b8b8e6ae4aa
morita <moritasoushi@gmail.com>
2022-05-20 09:38:11 +0900

    chore(lsp): remove unused import

--- home/.config/nvim/lua/moritasoshi/lsp/settings/jsonls.lua
+++ home/.config/nvim/lua/moritasoshi/lsp/settings/jsonls.lua
@@ -1,23 +1,16 @@
-local schemas = {}
-local status_ok, jsonls_settings = pcall(require, "nlspsettings.jsonls")
-if status_ok then
-  schemas = jsonls_settings.get_default_schemas()
-end
-
-schemas = vim.tbl_deep_extend("force", schemas, require("schemastore").json.schemas())
-
 local opts = {
-  cmd = { "nodebrew", "exec", "latest", "vscode-json-language-server", "--stdio"},
+  cmd = { "nodebrew", "exec", "latest", "vscode-json-language-server", "--stdio" },
   settings = {
     json = {
-      schemas = schemas,
+      schemas = require("schemastore").json.schemas(),
+      validate = { enable = true },
     },
   },
   setup = {
     commands = {
       Format = {
         function()
-          vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
+          vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
         end,
       },
     },
