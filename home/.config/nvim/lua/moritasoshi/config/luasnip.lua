2bdac19e57a18727656aef7cee15d23ed74e4f75
morita <moritasoushi@gmail.com>
2022-05-31 16:10:54 +0900

    feat: add some snippets

--- home/.config/nvim/lua/moritasoshi/config/luasnip.lua
+++ home/.config/nvim/lua/moritasoshi/config/luasnip.lua
@@ -3,11 +3,16 @@ if not present then
   return
 end
 
-
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
+-- YYYYMMDD for all
+ls.add_snippets("all", {
+  s("dateYMD", { t(os.date("%Y%m%d")) }),
+})
+
+-- console.log for javascript
 ls.add_snippets("javascript", {
   s("cl", {
     t("console.log("),
@@ -15,9 +20,7 @@ ls.add_snippets("javascript", {
     t(")"),
   }),
 })
-
 ls.filetype_extend("typescript", { "javascript" })
 ls.filetype_extend("typescriptreact", { "javascript" })
-
 
 require("luasnip.loaders.from_vscode").lazy_load() -- Activate friendly snippets
