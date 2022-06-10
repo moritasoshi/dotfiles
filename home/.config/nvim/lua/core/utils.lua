4f5467b366af4532286d211433dd9b660e05083d
morita <moritasoushi@gmail.com>
2022-06-10 17:02:16 +0900

    fix: remove unused functions

--- home/.config/nvim/lua/core/utils.lua
+++ home/.config/nvim/lua/core/utils.lua
@@ -2,64 +2,40 @@ _G.moriso = {}
 
 -- load plugin after entering vim ui
 moriso.packer_lazy_load = function(plugin, timer)
-   if plugin then
-      timer = timer or 0
-      vim.defer_fn(function()
-         require("packer").loader(plugin)
-      end, timer)
-   end
-end
-
-moriso.close_buffer = function(force)
-   if vim.bo.buftype == "terminal" then
-      vim.api.nvim_win_hide(0)
-      return
-   end
-
-   local fileExists = vim.fn.filereadable(vim.fn.expand "%p")
-   local modified = vim.api.nvim_buf_get_option(vim.fn.bufnr(), "modified")
-
-   -- if file doesnt exist & its modified
-   if fileExists == 0 and modified then
-      print "no file name? add it now!"
-      return
-   end
-
-   force = force or not vim.bo.buflisted or vim.bo.buftype == "nofile"
-
-   -- if not force, change to prev buf and then close current
-   local close_cmd = force and ":bd!" or ":bp | bd" .. vim.fn.bufnr()
-   vim.cmd(close_cmd)
+  if plugin then
+    timer = timer or 0
+    vim.defer_fn(function()
+      require("packer").loader(plugin)
+    end, timer)
+  end
 end
 
 -- keymap
 moriso.map = function(mode, keys, command, opt)
-   local options = { noremap = true, silent = true }
+  local options = { noremap = true, silent = true }
+  if opt then
+    options = vim.tbl_extend("force", options, opt)
+  end
 
-   if opt then
-      options = vim.tbl_extend("force", options, opt)
-   end
+  if type(keys) == "table" then
+    for _, keymap in ipairs(keys) do
+      moriso.map(mode, keymap, command, opt)
+    end
+    return
+  end
 
-   if type(keys) == "table" then
-      for _, keymap in ipairs(keys) do
-         moriso.map(mode, keymap, command, opt)
-      end
-      return
-   end
-
-   vim.keymap.set(mode, keys, command, opt)
+  vim.keymap.set(mode, keys, command, opt)
 end
 
 -- Convert table structure
 moriso.convert_plugin_structure = function(default_plugins)
-   local final_table = {}
+  local final_table = {}
 
-   for key, _ in pairs(default_plugins) do
-      default_plugins[key][1] = key
+  for key, _ in pairs(default_plugins) do
+    default_plugins[key][1] = key
 
-      final_table[#final_table + 1] = default_plugins[key]
-   end
+    final_table[#final_table + 1] = default_plugins[key]
+  end
 
-   return final_table
+  return final_table
 end
-
