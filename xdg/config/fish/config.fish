f034e8a9b588c03620db4b0e57ecda482f6685b8
morita <moritasoushi@gmail.com>
2022-07-13 14:35:18 +0900

    fix(fish): fix command history

--- xdg/config/fish/config.fish
+++ xdg/config/fish/config.fish
@@ -45,18 +45,44 @@ function fish_prompt
   echo -n '$ '
 end
 
-function moriso_repository -d 'Repository search'
+function moriso_repository -d 'Show repository'
   ls $HOME/src | fzf --reverse --height=100% | read select
   [ -n "$select" ]; and cd "$HOME/src/$select"
   echo " $select"
   commandline -f repaint
 end
 
-function moriso_history -d 'Command history search'
-  history merge
-  history | fzf --reverse --height=100% | read -lz result
-  and commandline $result
+function moriso_history -d 'Show command history'
+  test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
+  begin
+    set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT $FZF_DEFAULT_OPTS --tiebreak=index --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS +m"
+    set -l FISH_MAJOR (echo $version | cut -f1 -d.)
+    set -l FISH_MINOR (echo $version | cut -f2 -d.)
+
+    # history's -z flag is needed for multi-line support.
+    # history's -z flag was added in fish 2.4.0, so don't use it for versions
+    # before 2.4.0.
+    if [ "$FISH_MAJOR" -gt 2 -o \( "$FISH_MAJOR" -eq 2 -a "$FISH_MINOR" -ge 4 \) ];
+      history -z | eval (__fzfcmd) --read0 --print0 -q '(commandline)' | read -lz result
+      and commandline -- $result
+    else
+      history | eval (__fzfcmd) -q '(commandline)' | read -l result
+      and commandline -- $result
+    end
+  end
   commandline -f repaint
+end
+
+function __fzfcmd
+  test -n "$FZF_TMUX"; or set FZF_TMUX 1
+  test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
+  if [ -n "$FZF_TMUX_OPTS" ]
+    echo "fzf-tmux $FZF_TMUX_OPTS -- "
+  else if [ $FZF_TMUX -eq 1 ]
+    echo "fzf-tmux -d$FZF_TMUX_HEIGHT -- "
+  else
+    echo "fzf"
+  end
 end
 
 function fish_user_key_bindings
