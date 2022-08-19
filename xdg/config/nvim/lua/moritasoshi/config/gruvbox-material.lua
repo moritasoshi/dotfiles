1c7a0316bdae88d7f6d3ab246c5db3bbab89b4e7
morita <moritasoushi@gmail.com>
2022-08-19 21:48:59 +0900

    fix(nvim): Set vim.opt.background=dark

--- xdg/config/nvim/lua/moritasoshi/config/gruvbox-material.lua
+++ xdg/config/nvim/lua/moritasoshi/config/gruvbox-material.lua
@@ -1,5 +1,8 @@
 vim.opt.termguicolors = true
 
+-- For dark version
+vim.opt.background = "dark"
+
 vim.g.gruvbox_material_palette = "mix" -- material, mix, original
 vim.g.gruvbox_material_background = "hard" -- soft. medium, hard
 vim.g.gruvbox_material_enable_italic = 1
