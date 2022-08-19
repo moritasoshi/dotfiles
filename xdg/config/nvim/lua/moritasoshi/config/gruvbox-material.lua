503f302ff2ad680a0b2f208defd78548869522a8
morita <moritasoushi@gmail.com>
2022-08-19 22:20:30 +0900

    perf(nvim): Reduce roading time of color scheme

--- xdg/config/nvim/lua/moritasoshi/config/gruvbox-material.lua
+++ xdg/config/nvim/lua/moritasoshi/config/gruvbox-material.lua
@@ -6,5 +6,9 @@ vim.opt.background = "dark"
 vim.g.gruvbox_material_palette = "mix" -- material, mix, original
 vim.g.gruvbox_material_background = "hard" -- soft. medium, hard
 vim.g.gruvbox_material_enable_italic = 1
+
+-- For reducing loading time
+vim.g.gruvbox_material_better_performance = 1
+
 vim.cmd([[colorscheme gruvbox-material]])
 vim.cmd([[highlight CursorLine guibg=#393939]])
