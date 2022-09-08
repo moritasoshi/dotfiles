cd74e582f394a4f8a5fc8c6137b0b033908f3507
morita <moritasoushi@gmail.com>
2022-09-08 19:54:08 +0900

    fix: Fix bug

--- xdg/config/nvim/lua/moritasoshi/config/dap/nvim-dap.lua
+++ xdg/config/nvim/lua/moritasoshi/config/dap/nvim-dap.lua
@@ -1,20 +1,6 @@
-local present, dap = pcall(require, "dap")
+local present, _ = pcall(require, "dap")
 if not present then
   return
 end
 
-dap.adapters.go = {
-  type = 'executable';
-  command = 'node';
-  args = { os.getenv('HOME') .. '/src/vscode-go/dist/debugAdapter.js' };
-}
-dap.configurations.go = {
-  {
-    type = 'go';
-    name = 'Debug';
-    request = 'launch';
-    showLog = false;
-    program = "${file}";
-    dlvToolPath = vim.fn.exepath('/Users/morita/.local/bin/dlv') -- Adjust to where delve is installed
-  },
-}
+-- configured in `nvim-dap-go`
