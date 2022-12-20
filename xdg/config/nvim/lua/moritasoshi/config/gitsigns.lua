067f184dc9caa6368496fc64e552e17137121ad9
morita <moritasoushi@gmail.com>
2022-12-20 14:54:22 +0900

    fix: gitsigns

--- xdg/config/nvim/lua/moritasoshi/config/gitsigns.lua
+++ xdg/config/nvim/lua/moritasoshi/config/gitsigns.lua
@@ -19,6 +19,8 @@ gitsigns.setup {
   },
 
   on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
     local function map(mode, l, r, opts)
       opts = opts or {}
       opts.buffer = bufnr
@@ -26,26 +28,44 @@ gitsigns.setup {
     end
 
     -- Navigation
-    map("n", "]c", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-    map("n", "[c", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+    map("n", "]c", function()
+      if vim.wo.diff then
+        return "]c"
+      end
+      vim.schedule(function()
+        gs.next_hunk()
+      end)
+      return "<Ignore>"
+    end, { expr = true })
+
+    map("n", "[c", function()
+      if vim.wo.diff then
+        return "[c"
+      end
+      vim.schedule(function()
+        gs.prev_hunk()
+      end)
+      return "<Ignore>"
+    end, { expr = true })
 
     -- Actions
     map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
     map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
-    map("n", "<leader>hS", "<cmd>Gitsigns stage_buffer<CR>")
-    map("n", "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<CR>")
-    map("n", "<leader>hR", "<cmd>Gitsigns reset_buffer<CR>")
-    map("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>")
-    map("n", "<leader>hb", '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
-    map("n", "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<CR>")
-    map("n", "<leader>hd", "<cmd>Gitsigns diffthis<CR>")
-    map("n", "<leader>hD", '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-    map("n", "<leader>td", "<cmd>Gitsigns toggle_deleted<CR>")
+    map("n", "<leader>hS", gs.stage_buffer)
+    map("n", "<leader>hu", gs.undo_stage_hunk)
+    map("n", "<leader>hR", gs.reset_buffer)
+    map("n", "<leader>hp", gs.preview_hunk)
+    map("n", "<leader>hb", function()
+      gs.blame_line { full = true }
+    end)
+    map("n", "<leader>tb", gs.toggle_current_line_blame)
+    map("n", "<leader>hd", gs.diffthis)
+    map("n", "<leader>hD", function()
+      gs.diffthis("~")
+    end)
+    map("n", "<leader>td", gs.toggle_deleted)
 
     -- Text object
     map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
-
-    -- Command
-    vim.api.nvim_create_user_command("GitBlameToggle", ":Gitsigns toggle_current_line_blame", {})
   end,
 }
