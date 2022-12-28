eadca7d87c44ae45c01aa1f0cfe5865c0e715fe9
morita <moritasoushi@gmail.com>
2022-12-28 16:07:02 +0900

    refactor(nvim): remove unused autocommand

--- xdg/config/nvim/lua/core/init.lua
+++ xdg/config/nvim/lua/core/init.lua
@@ -1,13 +1,5 @@
 local autocmd = vim.api.nvim_create_autocmd
 
--- Disable statusline in dashboard
-autocmd("FileType", {
-  pattern = "alpha",
-  callback = function()
-    vim.opt.laststatus = 0
-  end,
-})
-
 -- Disable auto-trim in Java
 autocmd("FileType", {
   pattern = "java",
