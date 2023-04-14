c9f3c9f6c68c88258ccd8b716ec27a0c6e773b9f
Soshi Morita <moritasoushi@gmail.com>
2023-04-14 11:43:43 +0900

    feat: add tabular pattern

--- xdg/config/nvim/lua/moritasoshi/config/tabular.lua
+++ xdg/config/nvim/lua/moritasoshi/config/tabular.lua
@@ -1,6 +1,7 @@
 vim.cmd([[
 AddTabularPattern! nvar /nvarchar(\w*)/l1r0
 AddTabularPattern! f_comma /^[^,]*\zs,/l0l1
+AddTabularPattern! comma /,\zs,/l0l1
 AddTabularPattern! f_colon /^[^:]*\zs:\zs/l0r1
 AddTabularPattern! f_equal /^[^=]*\zs=/
 AddTabularPattern! f_quote /^[^"]*\zs"/l1r0
