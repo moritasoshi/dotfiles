b75f4cd1ec3b44e9e7a77329e8b5bdf1448b08de
morita <moritasoushi@gmail.com>
2023-04-21 23:03:22 +0900

    chore: sort

--- xdg/config/nvim/lua/moritasoshi/plugins_lazy.lua
+++ xdg/config/nvim/lua/moritasoshi/plugins_lazy.lua
@@ -251,15 +251,15 @@ return {
     "rafamadriz/friendly-snippets",
     event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
-      "hrsh7th/nvim-cmp",
       "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-cmdline",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-path",
+      "hrsh7th/nvim-cmp",
       "onsails/lspkind-nvim",
-      "saadparwaiz1/cmp_luasnip",
       { "tzachar/cmp-tabnine", build = "./install.sh" },
     },
     config = function()
