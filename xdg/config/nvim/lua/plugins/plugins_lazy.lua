28a51c83515c527ea37f805ae2176f6119bfac88
morita <moritasoushi@gmail.com>
2023-11-18 18:15:03 +0900

    feat: luasnip&cmp

--- xdg/config/nvim/lua/plugins/plugins_lazy.lua
+++ xdg/config/nvim/lua/plugins/plugins_lazy.lua
@@ -187,33 +187,6 @@ return {
     ft = "go",
   },
 
-  -- Completion
-  {
-    "rafamadriz/friendly-snippets",
-    event = { "InsertEnter", "CmdlineEnter" },
-    dependencies = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-emoji",
-      {
-        "onsails/lspkind-nvim",
-        config = function()
-          require("plugins.config.lspkind")
-        end,
-      },
-      "saadparwaiz1/cmp_luasnip",
-      { "L3MON4D3/LuaSnip", version = "1.*", run = "make install_jsregexp" },
-      { "tzachar/cmp-tabnine", build = "./install.sh", dependencies = "hrsh7th/nvim-cmp" },
-    },
-    config = function()
-      require("plugins.config.luasnip")
-      require("plugins.config.cmp")
-    end,
-  },
-
   -- Telescope
   {
     "nvim-telescope/telescope.nvim",
