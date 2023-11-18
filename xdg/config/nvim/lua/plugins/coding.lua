c2e303d36a3486a0c782acb5822b91bdb8cc2fa3
morita <moritasoushi@gmail.com>
2023-11-18 18:03:29 +0900

    feat: supertab

---
+++ xdg/config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,41 @@
+-- Snippets and auto completions
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load() -- Activate friendly snippets
+    end,
+    dependencies = { "rafamadriz/friendly-snippets" },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+          else
+            fallback()
+          end
+        end,
+        ["<S-Tab>"] = function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+          else
+            fallback()
+          end
+        end,
+      })
+    end,
+  },
+}
