d71a00757d3a72997e3c276add57eb3410f924c7
morita <moritasoushi@gmail.com>
2023-11-18 19:57:44 +0900

    chore: update

--- xdg/config/nvim/lua/plugins/config/dap/dap-go.lua
+++
@@ -1,6 +0,0 @@
-local present, dapgo = pcall(require, "dap-go")
-if not present then
-  return
-end
-
-dapgo.setup()
--- xdg/config/nvim/lua/plugins/plugins_lazy.lua
+++ xdg/config/nvim/lua/plugins/plugins_lazy.lua
@@ -2,10 +2,6 @@ if true then
   return {}
 end
 
-local m = {
-  dap_enabled = false,
-}
-
 return {
   -- Text manipulation
   {
@@ -24,58 +20,15 @@ return {
       require("plugins.config.tabout")
     end,
   },
-  -- indent guidelines
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-  },
-
-  -- colorizer
-  {
-    "NvChad/nvim-colorizer.lua",
-    event = "BufReadPre",
-    config = true,
-  },
-  "nvim-lua/plenary.nvim",
 
   -- UI & Interface
-  { "nvim-tree/nvim-web-devicons", config = true },
   {
     "nvim-lualine/lualine.nvim",
     config = function()
       require("plugins.config.lualine")
     end,
   },
-  "ryanoasis/vim-devicons",
   "simeji/winresizer",
-
-  -- Git
-  -- ["f-person/git-blame.nvim"] ,
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- required
-      "nvim-telescope/telescope.nvim", -- optional
-      "sindrets/diffview.nvim", -- optional
-    },
-    cmd = { "Neogit" },
-    config = true,
-  },
-  {
-    "akinsho/git-conflict.nvim",
-    version = "*",
-    config = function()
-      require("plugins.config.git-conflict")
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("plugins.config.gitsigns")
-    end,
-  },
-  "rhysd/committia.vim",
 
   -- LSP
   {
@@ -88,74 +41,12 @@ return {
 
   "neovim/nvim-lspconfig",
   "b0o/schemastore.nvim",
-  "folke/neodev.nvim",
   {
     "j-hui/fidget.nvim",
     tag = "legacy",
     config = function()
       require("plugins.config.fidget")
     end,
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    config = function()
-      require("plugins.config.lsp.null-ls")
-    end,
-  },
-  {
-    "folke/trouble.nvim",
-    config = function()
-      require("plugins.config.trouble")
-    end,
-    cmd = { "TroubleToggle" },
-  },
-
-  -- Debug Adapter
-  {
-    "mfussenegger/nvim-dap",
-    config = function()
-      require("plugins.config.dap/nvim-dap")
-    end,
-    enabled = m.dap_enabled,
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    config = function()
-      require("plugins.config.dap/dapui")
-    end,
-    enabled = m.dap_enabled,
-  },
-  {
-    "theHamsta/nvim-dap-virtual-text",
-    config = function()
-      require("plugins.config.dap/nvim-dap-virtual-text")
-    end,
-    enabled = m.dap_enabled,
-  },
-  {
-    "leoluz/nvim-dap-go",
-    config = function()
-      require("plugins.config.dap/dap-go")
-    end,
-    enabled = m.dap_enabled,
-    ft = "go",
-  },
-
-  -- Go Development
-  {
-    "ray-x/go.nvim",
-    config = function()
-      require("plugins.config.go")
-    end,
-    ft = "go",
-  },
-  {
-    "ray-x/navigator.lua",
-    dependencies = {
-      { "ray-x/guihua.lua", build = "cd lua/fzy && make" },
-      { "neovim/nvim-lspconfig" },
-    },
-    ft = "go",
   },
 
   -- HTTP Client
