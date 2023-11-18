0ce471d578f44fc08fe0ace4dbef0ae43bef8f20
morita <moritasoushi@gmail.com>
2023-11-18 20:46:39 +0900

    chore: migrate keymaps

--- xdg/config/nvim/lua/config/keymaps.lua
+++ xdg/config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,40 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = function(mode, keys, command, opt)
+  local options = { noremap = true, silent = true }
+  if opt then
+    options = vim.tbl_extend("force", options, opt)
+  end
+
+  vim.keymap.set(mode, keys, command, opt)
+end
+
+map("n", "<Space>//", ":%s/")
+map("n", "<S-CR>", ":<C-u>call append(expand('.'), '')<CR>j")
+map("", "go", "<C-O>")
+
+-- map("n", "gy", ":let @* = expand('%:t') | lua print('Copied ' .. vim.fn.expand('%:t') .. ' to system clipboard!')<CR>")
+-- map("n", "gyy", ":let @* = expand('%') | lua print('Copied ' .. vim.fn.expand('%') .. ' to system clipboard!')<CR>")
+
+-- map("x", "p", "pgvy")
+
+-- Tabular
+-- map({ "n", "x" }, "<leader>t", ":Tabularize /")
+
+-- map("n", "<leader>rr", [[<cmd>lua require('plugins.config.rest-nvim').do_rest()<cr>]])
+-- map("n", "<leader>rp", [[<cmd>lua require('plugins.config.rest-nvim').view_rest()<cr>]])
+
+-- simeji/winresizer
+-- map("", "<LEFT>", ":WinResizerStartResize<CR>h<CR>")
+-- map("", "<DOWN>", ":WinResizerStartResize<CR>j<CR>")
+-- map("", "<UP>", ":WinResizerStartResize<CR>k<CR>")
+-- map("", "<RIGHT>", ":WinResizerStartResize<CR>l<CR>")
+
+-- terminal mode
+vim.cmd([[
+tnoremap <Esc> <C-\><C-n>
+tnoremap <A-h> <C-\><C-N><C-w>h
+tnoremap <A-j> <C-\><C-N><C-w>j
+tnoremap <A-k> <C-\><C-N><C-w>k tnoremap <A-l> <C-\><C-N><C-w>l
+]])
