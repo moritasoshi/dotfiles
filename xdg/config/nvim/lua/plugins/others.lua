daa54266ba03bf9e49635936b6927fc50a432b30
Soshi Morita <moritasoushi@gmail.com>
2024-06-11 20:24:58 +0900

    chore(nvim): use neovim v0.10.0

--- xdg/config/nvim/lua/plugins/others.lua
+++ xdg/config/nvim/lua/plugins/others.lua
@@ -12,7 +12,17 @@ return {
 
   -- HTTP Client
   {
-    "NTBBloodbath/rest.nvim",
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+    },
+  },
+  {
+    "rest-nvim/rest.nvim",
+    ft = "http",
+    dependencies = { "luarocks.nvim" },
     opts = {
       -- Open request results in a horizontal split
       result_split_horizontal = false,
@@ -30,7 +40,9 @@ return {
       { "<leader>rr", "<Plug>RestNvim", desc = "Run request under cursor" },
       { "<leader>rp", "<Plug>RestNvimPreview", desc = "Preview request cURL command" },
     },
-    ft = "http",
+    config = function()
+      require("rest-nvim").setup()
+    end,
   },
 
   -- Optional (Trial)
