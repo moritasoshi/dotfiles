a6e0b6c183bdb7fb5827d3e33019396a07583f9e
Soshi Morita <66908774+moritasoshi@users.noreply.github.com>
2024-10-03 11:11:59 +0900

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -12,7 +12,6 @@ git clone https://github.com/moritasoshi/dotfiles.git ~/src/dotfiles >/dev/null
 cd ~/src/dotfiles
 git fetch && git checkout feature/one-line
 git remote set-url origin git@github.com:moritasoshi/dotfiles.git
-cd -
 
 echo "Installation starting..."
 ~/src/dotfiles/bootstrap.sh -i
