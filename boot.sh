67d9ab3f8ddb4bcb2e3a36b25a9dd48d3d0c2eef
Soshi Morita <moritasoushi@gmail.com>
2024-10-03 11:07:25 +0900

    wip

--- boot.sh
+++ boot.sh
@@ -8,7 +8,11 @@ sudo apt-get install -y git >/dev/null
 echo "Cloning Dotfiles..."
 rm -rf ~/src/dotfiles
 git clone https://github.com/moritasoshi/dotfiles.git ~/src/dotfiles >/dev/null
+
+cd ~/src/dotfiles
+git fetch && git checkout feature/one-line
 git remote set-url origin git@github.com:moritasoshi/dotfiles.git
+cd -
 
 echo "Installation starting..."
 ~/src/dotfiles/bootstrap.sh -i
